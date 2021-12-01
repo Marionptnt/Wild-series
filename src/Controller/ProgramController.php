c6c8aa1fe88ba9aa35866f25b7fe78b721dd48e2
Marionptnt <dev.marionpatinet@gmail.com>
2021-12-01 22:10:42 +0100

    quest 5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -32,4 +32,41 @@ class ProgramController extends AbstractController
 
         ]);
     }
+
+    /* écriture requirements plus condensé-->
+
+    /**
+     
+     * @Route("/program/list/{page<\d+>}", name="program_list")
+     
+     */
+    public function list(int $page = 1): Response
+    {
+        return $this->render('program/list.html.twig', [
+            'page' => $page,
+        ]);
+    }
+
+    /*
+    public function new(): Response
+{
+    // traitement d'un formulaire par exemple
+ 
+    // redirection vers la page 'program_show',
+    // correspondant à l'url /program/4
+    return $this->redirectToRoute('program_show', ['id' => 4]);
+}
+
+
+    /**
+
+     * @Route("/program/{id}",methods={"GET"}, requirements={"id"="\d+"}, name="program_show")
+
+     */
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
 }
